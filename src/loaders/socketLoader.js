6ab1e7bb3b16b13d5d64d439a63f65b84cdd47a2
Jan Vodenka <vodenkaj3@gmail.com>
2021-06-28 09:38:33 +0200

    cleanup

--- src/loaders/socketLoader.js
+++ src/loaders/socketLoader.js
@@ -5,9 +5,13 @@ import {config as cfg} from "../config.js";
 
 function SocketLoader(app) {
 
+	// New socket io instance
 	const io = new socketio.Server(app.server);
 
+	// Event fired on every connection made
 	io.on("connection", (socket) => {
+		
+		// Getting session id to check if client is logged in
 		const id = qs.parse(socket.request.headers["cookie"]).sessionid;
 		if (id && app.session.has(id)) {
 			io.to(socket.id).emit("logged", app.session.get(id).username);
@@ -20,21 +24,29 @@ function SocketLoader(app) {
 			socket.disconnect();
 		}
 
+		// Event fired on client message
 		socket.on("chat message", msg => {
+			
+			// Getting session id to check if client is logged in
 			const id = qs.parse(socket.request.headers["cookie"]).sessionid;
-			if (msg.length > cfg.MAX_LEN_MSG) {
-				io.to(socket.id).emit("exceeded limit", {username: msg.username, text: msg.text});
-				return;
-			}
-			else if (!id || !app.session.has(id)) {
+
+			if (!id || !app.session.has(id)) {
 				io.to(socket.id).emit("disconnected", "You have been disconected, please refresh the page.");
 				return;
 			}
+			else if (msg.length > cfg.MAX_LEN_MSG) {
+				io.to(socket.id).emit("exceeded limit", {username: msg.username, text: msg.text});
+				return;
+			}
 
+			// Sanitize the messages to get rid of possible XSS attack
 			msg = validator.escape(msg);
+
 			io.emit("chat message", {username: app.session.get(id).username, text: msg});
 			if (app.store.length == cfg.MAX_MSGS)
 				app.store.shift();
+
+			// Store the message
 			app.store.push({username: app.session.get(id).username, text: msg});
 		});
 	});
