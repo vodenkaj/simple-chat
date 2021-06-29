93b9a809b80818423d7d847def6e00e806b00bb9
vodenkaj <67589938+vodenkaj@users.noreply.github.com>
2021-06-29 12:21:14 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,4 +4,4 @@ config.ADDRESS = "localhost";
 config.PORT = 4500;
 config.MAX_MSGS = 10; // Size of the array for storing recent messages
 config.MAX_LEN_MSG = 500; // Maximum length of one message
-config.MSG_TIME_LIMIT = 5000; // Time before client cant send a new message
+config.MSG_TIME_LIMIT = 5000; // Time before client can send a new message
